db1a3d88b3b7d73e13c405c4456cce8d25fb1db9
Victoria Daug <victoria.daug@gmail.com>
2018-03-05 19:45:22 +0000

    Echo endpoint

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 
 // your code here!
-
+app.get('/echo/:what', function (req, res) {
+  res.json({
+    host: req.hostname,
+    query: req.query,
+    params: req.params
+  })
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
